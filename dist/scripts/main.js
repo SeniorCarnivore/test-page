8966c63a315b7dc78eb44e3f7d350634bdc07d01
Kirill Fadeev <fk@grapheme.ru>
2016-12-04 18:02:18 +0300

    cuz we've got pictures here

---
+++ dist/scripts/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(function () {
+  $('.person-button').on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('unfollow');
+  });
+});
+//# sourceMappingURL=main.js.map
